a675c3af384635bb28262185e70ebd47a2dbb84f
Harald Zealot <harald_zealot@tut.by>
2015-04-29 21:17:34 +0300

    [+] exand worldview

--- include/worldview.h
+++ include/worldview.h
@@ -3,18 +3,32 @@
 
 #include <QPoint>
 
-class WorldView
+class WorldView final
 {
 public:
-    //G VI default by time
+    WorldView(int mapLeft, int mapRight, int mapTop, int mapBottom,
+              int screenLeft, int screenRight, int screenTop, int screenBottom,
+              qreal scale);
+    WorldView(const WorldView &) = default;
+    WorldView &operator=(const WorldView &) = default;
+    WorldView(WorldView &&) = default;
+    WorldView &operator =(WorldView &&) = default;
+    ~WorldView() = default;
 
     void moveScreen(QPoint screenShift);
-    void moveScreen(int x, int y){moveScreen(QPoint(x,y));}
+    void moveScreen(int x, int y)
+    {
+        moveScreen(QPoint(x, y));
+    }
+
+    void decreaseScale();
+    void increaseScale();
 private:
     int mapLeft, mapRight, mapTop, mapBottom;
     int screenLeft, screenRight, screenTop, screenBottom;
     QPoint currentCenter; //in map coordinate
     qreal scale;
+    qreal minimalScale, maximalScale;
 };
 
 #endif // WORLDVIEW_H
--- src/worldview.cpp
+++ src/worldview.cpp
@@ -4,6 +4,24 @@
 using namespace std;
 
 
+WorldView::WorldView(int mapLeft, int mapRight, int mapTop, int mapBottom,
+                     int screenLeft, int screenRight, int screenTop, int screenBottom,
+                     qreal scale):
+    mapLeft(mapLeft),
+    mapRight(mapRight),
+    mapTop(mapTop),
+    mapBottom(mapBottom),
+    currentCenter((mapLeft + mapRight) / 2, (mapTop + mapBottom) / 2),
+    scale(scale),
+    maximalScale(10.0)// TODO without magic number
+{
+    auto horizontalMinimalScale = (qreal)(mapRight - mapLeft)
+                                  / (qreal)(screenRight - screenLeft);
+    auto verticalMinimalScale = (qreal)(mapTop - mapBottom)
+                                / (qreal)(screenTop - screenBottom);
+    minimalScale = max(horizontalMinimalScale, verticalMinimalScale);
+}
+
 void WorldView::moveScreen(QPoint screenShift)
 {
     QPoint mapShift = screenShift / scale;
@@ -15,4 +33,18 @@ void WorldView::moveScreen(QPoint screenShift)
         min(min(boxRight, max(boxLeft, mapShift.x())) / (qreal)mapShift.x(),
             min(boxTop, max(boxBottom, mapShift.y())) / (qreal)mapShift.y());
     currentCenter += quotient * mapShift;
+}
+
+void WorldView::decreaseScale()
+{
+    //TODO
+}
+
+void WorldView::increaseScale()
+{
+    auto futureScale = scale * 1.01;
+    if(futureScale > maximalScale)
+        scale = maximalScale;
+    else
+        scale = futureScale;
 }
