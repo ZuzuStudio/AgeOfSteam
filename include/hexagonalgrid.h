fc2728484ce9535ce431139781bdd610b3c0e82e
Harald Zealot <harald_zealot@tut.by>
2015-05-06 23:40:46 +0300

    [+] new hexagonal grid

--- include/hexagonalgrid.h
+++ include/hexagonalgrid.h
@@ -1,20 +1,24 @@
 #ifndef HEXAGONALGRID_H
 #define HEXAGONALGRID_H
 
-//#include "include/hexagon.h"
-
-#include <QPoint>
 #include <QPointF>
+#include <QPoint>
 
 class HexagonalGrid
 {
 public:
-    HexagonalGrid(int colls, int rows, int hexagonSize);
+    HexagonalGrid(int cols, int rows, int hexagonBigRadius);
     //TODO GVI
 
-    QPointF cellCenter(int collumnIndex, int rowIndex)const;
-    int collumn(QPointF point)const;
-    int row(QPointF point)const;
+    QPointF cellCenter(int columnIndex, int rowIndex)const;
+
+    QPointF northWestCorner(int columnIndex, int rowIndex)const
+    {
+        return cellCenter(columnIndex, rowIndex)
+               - QPointF(floatHexagonSmallRadius, floatHexagonBigRadius);
+    }
+
+    QPoint indices(QPointF point, QPointF directionToView)const;
 
     int leftMapBorder()const
     {
@@ -37,57 +41,12 @@ public:
     }
 
 private:
+    int cols, rows;
+    int hexagonBigRadius;
+    qreal floatHexagonBigRadius, floatHexagonSmallRadius;
+    qreal horizontalStep, verticalStep, oddShift;
     int left, right, top, bottom;
+
 };
-
-/*
-
-class HexagonalGrid
-{
-
-public:
-
-    HexagonalGrid(qreal scale, QPoint topLeft, QPoint topRight, QPoint bottomRight, QPoint bottomLeft);
-    ~HexagonalGrid();
-    std::vector<QImage> drawRastr(QSvgRenderer * renderer);
-    void drawSVG(QSvgRenderer * renderer, QPainter * painter);
-    void gluingTogetherClasters(QPainter * painter);
-    void setScale(qreal scale);
-    void addShift(int x, int y);
-    void addShift(QPoint pos);
-
-private:
-
-    class ArrayGrid
-    {
-    private:
-        int **array;
-        size_t rows, colomns;
-
-    public:
-        ArrayGrid(size_t rows, size_t colomns);
-        ~ArrayGrid();
-        int ** createGrid();
-
-    }* grid;
-
-    void calculateScreenCoordinates();
-
-    QSize image_size;
-    Hexagon *hexagon;
-    qreal scale;
-    int **matrix;
-    std::vector<QImage> painters;
-
-    QPoint shift;
-    QPoint mapLeftTop, mapRightTop, mapRightBottom, mapLeftBottom;
-    QPoint screenLeftTop, screenRightTop, screenRightBottom, screenLeftBottom;
-    WorldView worldView;
-    const size_t numberOfClasters = 25;
-    const size_t sizeOfClaster = 20;  // from now on it is important
-                                     // to use only clusters which size is odd integer
-};
-
-*/
 
 #endif // HEXAGONALGRID_H
--- src/hexagonalgrid.cpp
+++ src/hexagonalgrid.cpp
@@ -1,213 +1,78 @@
 ﻿#include "include/hexagonalgrid.h"
 
-/*HexagonalGrid::ArrayGrid::ArrayGrid(size_t rows, size_t colomns):rows(rows), colomns(colomns)
+#include <math.h>
+#include <assert.h>
+
+HexagonalGrid::HexagonalGrid(int cols, int rows, int hexagonBigRadius):
+    cols(cols),
+    rows(rows),
+    hexagonBigRadius(hexagonBigRadius),
+    floatHexagonBigRadius((qreal)hexagonBigRadius),
+    floatHexagonSmallRadius(sqrt(3.0) * floatHexagonBigRadius / 2.0),
+    horizontalStep(2.0 * floatHexagonSmallRadius),
+    verticalStep(1.5 * floatHexagonBigRadius),
+    oddShift(floatHexagonSmallRadius)
 {
-    array = new int*[rows];
-    for(size_t i = 0; i < rows; ++i)
-    {
-        array[i] = new int[colomns];
-    }
+    assert(cols > 0);
+    assert(rows > 0);
+    assert(rows & 1);
+    assert(hexagonBigRadius > 0);
+    auto width = (this->cols - 1) * horizontalStep;
+    auto height = (this->rows - 1) * verticalStep;
+    int halfWidth = (int)floor(width / 2.0);
+    int halfHeight = (int)floor(height / 2.0);
+    left = -halfWidth;
+    right = halfWidth;
+    top = halfHeight;
+    bottom = -halfHeight;
 }
 
-HexagonalGrid::ArrayGrid::~ArrayGrid()
+QPointF HexagonalGrid::cellCenter(int columnIndex, int rowIndex) const
 {
-    for(size_t i = 0 ; i < rows; ++i)
-    {
-        delete array[i];
-        array[i] = nullptr;
-    }
-    delete array;
-    array = nullptr;
+    return QPointF(left + columnIndex * horizontalStep + (rowIndex & 1) * oddShift,
+                   top - rowIndex * verticalStep);
 }
 
-/**
- * Now it just creates a simple matrix of 1's, but in nearest time it should also become
- * the base of generation of true map
-*/
-/*
-int ** HexagonalGrid::ArrayGrid::createGrid()
+QPoint HexagonalGrid::indices(QPointF point, QPointF directionToView) const
 {
-    for(size_t i = 0; i < rows; ++i)
-    {
-        for(size_t j = 0; j < colomns; ++j)
+    int approximateColumnIndex = (int)((point.x() - left) / horizontalStep);
+    int approximateRowIndex = (int)((top - point.y()) / verticalStep);
+    auto squareSearchRadius = (1.125 * floatHexagonBigRadius) * (1.125 * floatHexagonBigRadius);
+    QPointF candidateRadiusvector[3];
+    QPoint candidateIndices[3];
+    int candidateCount = 0;
+
+    for(int collumn = approximateColumnIndex - 2; collumn <= approximateColumnIndex + 2; ++collumn)
+        for(int row = approximateRowIndex - 2; row <= approximateRowIndex + 2; ++row)
         {
-            array[i][j] = 1;
-        }
-    }
-    return array;
-}
+            auto radiusVector = cellCenter(collumn, row) - point;
 
-HexagonalGrid::HexagonalGrid(qreal scale, QPoint topLeft, QPoint topRight, QPoint bottomRight, QPoint bottomLeft):
-    grid(nullptr),
-    hexagon(nullptr),
-    scale(scale),
-    shift(0, 0),
-    mapLeftTop(0, 0),
-    mapRightTop(0, 0),
-    mapRightBottom(0, 0),
-    mapLeftBottom(0, 0),
-    worldView(0,0,0,0,  topLeft.x(), bottomRight.x(), topLeft.y(), bottomRight.y(), scale)
-{
-    hexagon = new Hexagon(100);
-    grid = new ArrayGrid(20, 20);
-    screenLeftTop = topLeft;
-    screenRightTop = topRight;
-    screenRightBottom = bottomRight;
-    screenLeftBottom = bottomLeft;
-}
-
-HexagonalGrid::~HexagonalGrid()
-{
-    delete grid;
-    grid = nullptr;
-    delete hexagon;
-    hexagon = nullptr;
-}
-
-std::vector<QImage> HexagonalGrid::drawRastr(QSvgRenderer * renderer)
-{
-    int cornersX[6];
-    int cornersY[6];
-    hexagon->setCellIndex(0, 0);
-    hexagon->computeCorners(cornersX, cornersY);
-    image_size = QSize( (sizeOfClaster + 0.5) * (cornersX[1] - cornersX[5]),
-                        (sizeOfClaster / 2 + sizeOfClaster % 2) * (cornersY[3] - cornersY[0]) +
-                         (sizeOfClaster / 2 + 0.5) * (cornersY[2] - cornersY[1]));
-
-    for(size_t k = 0; k < numberOfClasters; ++k)
-    {
-        QImage image(image_size, QImage::Format_ARGB32_Premultiplied);
-        QPainter *painter = new QPainter(&image);
-        //painter->fillRect(0, 0, image_size.width(), image_size.height(), Qt::white);
-
-        for (size_t j = 0; j < sizeOfClaster; ++j)
-        {
-            for (size_t i = 0; i < sizeOfClaster; ++i)
+            if(QPointF::dotProduct(radiusVector, radiusVector) < squareSearchRadius)
             {
-                hexagon->setCellIndex(i, j);
-                hexagon->computeCorners(cornersX, cornersY);
-                renderer->render(painter, QRectF(cornersX[5], cornersY[0], cornersX[1] - cornersX[5], cornersY[3] - cornersY[0]));
+                candidateRadiusvector[candidateCount] = radiusVector;
+                candidateIndices[candidateCount] = QPoint(collumn, row);
+                ++candidateCount;
+                assert(candidateCount <= 3);
             }
         }
-        delete painter;
-        painters.push_back(image);
-    }
-    return painters;
-}
 
-void HexagonalGrid::gluingTogetherClasters(QPainter *p)
-{
-    calculateScreenCoordinates();
-    /*if(screenLeftTop.x() < 0)
-    {
-        shift.rx() = 0;
-    }
-    else if(screenLeftTop.y() < 0)
-    {
-        shift.ry() = 0;
-    }
-    else if(screenRightTop.x() > mapRightTop.x())
-    {
-        shift.rx() = mapRightTop.x();
-    }
-    else if(screenRightBottom.y() > mapRightBottom.y())
-    {
-        shift.ry() = mapRightBottom.y();
-    }*//*
-    size_t counter = 1;
-    int width = shift.x();
-    int height = shift.y();
-    p->drawImage(shift.x(), shift.y(), painters.at(0));
+    assert(candidateCount > 0);
 
-    int mod = (int) sqrt(numberOfClasters);
+    if(candidateCount == 1)
+        return candidateIndices[0];
 
-    while(counter < numberOfClasters)
+    int maxIndex = 0;
+    qreal maxValue = QPointF::dotProduct(candidateRadiusvector[maxIndex], directionToView);
+
+    for(int i = 1; i < candidateCount; ++i)
     {
-        if(counter % mod != 0)
+        qreal currrentValue = QPointF::dotProduct(candidateRadiusvector[i], directionToView);
+        if(currrentValue > maxValue)
         {
-            width += painters.at(0).size().width() - hexagon->getWidth() / 2 - (sizeOfClaster - 1);
-            p->drawImage(width,
-                         height,
-                         painters.at(counter));
-        }
-        else
-        {
-            height += painters.at(0).size().height() - hexagon->getHeight() * 0.25 - (sizeOfClaster - 1);
-            width = shift.x();
-            p->drawImage(width,
-                         height,
-                         painters.at(counter));
-        }
-        ++counter;
-    }
-    width += hexagon->getWidth();
-    height += hexagon->getHeight();
-    mapRightTop = QPoint(width, 0);
-    mapRightBottom = QPoint(width, height);
-    mapLeftBottom = QPoint(0, height);
-}
-
-void HexagonalGrid::calculateScreenCoordinates()
-{
-    screenLeftTop += shift;
-    screenRightTop += shift;
-    screenRightBottom += shift;
-    screenLeftBottom += shift;
-}
-
-void HexagonalGrid::setScale(qreal scale)
-{
-    this->scale = scale;
-}
-
-void HexagonalGrid::addShift(int x, int y)
-{
-    shift += QPoint(x,y) / scale;
-}
-
-void HexagonalGrid::addShift(QPoint pos)
-{
-    shift += pos / scale;
-}
-
-void HexagonalGrid::drawSVG(QSvgRenderer *renderer, QPainter *painter)
-{
-    int cornersX[6];
-    int cornersY[6];
-    Hexagon currentHexagon(100 * scale);
-    currentHexagon.setCellIndex(0, 0);
-    currentHexagon.computeCorners(cornersX, cornersY);
-    image_size = QSize( (sizeOfClaster / 2 + sizeOfClaster % 2) * (cornersX[2] - cornersX[5]) +
-                        (sizeOfClaster / 2  + (sizeOfClaster % 2 == 0 ? 0.5 : 0)) * (cornersX[1] - cornersX[0]),
-                         (sizeOfClaster + 0.5) * (cornersY[4] - cornersY[0]) );
-
-    int totalWidth = image_size.width();
-    unsigned width_cells = totalWidth / ( (cornersX[2] - cornersX[5]) * 0.75) + 2;
-
-    int totalHeight = image_size.height();
-    unsigned height_cells = totalHeight / (cornersY[4] - cornersY[0]) + 2;
-    for (size_t j = 0; j < height_cells; ++j)
-    {
-        for (size_t i = 0; i < width_cells; ++i)
-        {
-            currentHexagon.setCellIndex(i, j);
-            currentHexagon.computeCorners(cornersX, cornersY);
-            renderer->render(painter, QRectF(cornersX[5], cornersY[0],
-                    (cornersX[1] - cornersX[5]) + 1, (cornersY[3] - cornersY[0]) + 1));
+            maxValue = currrentValue;
+            maxIndex = i;
         }
     }
 
-    currentHexagon.setCellByPoint( - shift.y() * scale + painter->window().bottomRight().y(),
-                                   - shift.x() * scale + painter->window().bottomRight().x());
-    //qDebug() << "Mi = " << currentHexagon.getIndexI() << "\tMj = " << currentHexagon.getIndexJ();
-    painter->end();
-
-    /**
-     * Now on we gonna get all four points of current screen metrics, and they ought to reflect
-     * which clusters should be loaded. I suppose that we need to load each cluster corresponding
-     * for it's point. Hope so, the worst case is 4(8) clusters to load won't be a great ploblem
-     * to solve.
-    *//*
+    return candidateIndices[maxIndex];
 }
-*/
-
